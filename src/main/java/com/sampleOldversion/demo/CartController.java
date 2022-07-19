68fb7aa9bb66af51c3304a6e1e5cd6245159ac3b
aparna <aparna.06cs03@gmail.com>
2022-07-19 12:58:00 +0530

    changes

--- src/main/java/com/sampleOldversion/demo/CartController.java
+++ src/main/java/com/sampleOldversion/demo/CartController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.sampleOldversion.demo.service.CartService;
 
-@RefreshScope
 @RequestMapping("/cart")
 @RestController
 public class CartController {
--- src/main/java/com/sampleOldversion/demo/service/CartServiceImpl.java
+++ src/main/java/com/sampleOldversion/demo/service/CartServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
-@RefreshScope
 public class CartServiceImpl implements CartService {
 
 	@Value("${cart.success}")
